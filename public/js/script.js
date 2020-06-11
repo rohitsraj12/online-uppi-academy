01100c2e01e6a7c5103517dd1c4ddd36591e5142
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-11 17:13:44 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-
-    $("#hamberger").click(function () {
-      $(this).toggleClass("is__active");
-      $("#header-nav ul").slideToggle(400);
-    });
-    
+  $("#hamberger").click(function () {
+    $(this).toggleClass("is__active");
+    $("#header-nav ul").slideToggle(400);
   });
+
+  //hide all tabs first
+  $(".tab-content").hide();
+  //show the first tab content
+  $("#tab-1").show();
+
+  $("#select-box").change(function () {
+    dropdown = $("#select-box").val();
+    //first hide all tabs again when a new option is selected
+    $(".tab-content").hide();
+    //then show the tab content of whatever option value was selected
+    $("#" + "tab-" + dropdown).show();
+  });
+});
