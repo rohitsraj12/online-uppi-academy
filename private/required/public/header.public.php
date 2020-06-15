3fb39c9d668a11e3a08b465c1f7fc31cf1e76f63
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-15 22:03:43 +0530

    Create header.public.php

---
+++ private/required/public/header.public.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+  <!--<![endif]-->
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Home | uppionlineacademy.com</title>
+
+    <meta name="description" content="<?php //echo $meta_discription;?>" />
+    <meta name="keywords" content="<?php //echo $meta_key;?>" />
+
+    <meta name="author" content="Rohit, rohitwebco@gmail.com" />
+    <meta name="distribution" content="global" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta name="url" content="<?php // base_url();?>" />
+    <meta name="Classification" content="tuition institute" />
+    <meta name="owner" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="index, follow" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600&family=Open+Sans&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- style -->
+    <link
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      rel="stylesheet"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="<?php base_url();?>css/owl.carousel.css" />
+    <link rel="stylesheet" href="<?php base_url();?>css/owl.theme.default.css" />
+    <link rel="stylesheet" href="<?php base_url();?>css/style.css" />
+
+    <!-- script -->
+  </head>
+
+  <body>
+    <!--[if lt IE 7]>
+      <p class="browsehappy">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="#">upgrade your browser</a> to improve your experience.
+      </p>
+    <![endif]-->
+    <div class="body-wrap">
+      <div class="body-header">
+        <div class="body-header-top">
+          <div class="wrap-container">
+            <div class="crash-course">
+              <p class="body-header-text">
+                <strong class="primary-word">physics</strong> crash course for
+                <strong class="primary-word">IIT JEE/NEET</strong>
+              </p>
+            </div>
+            <div class="header-contact">
+              <ul>
+                <li class="header-contact__list">
+                  <a
+                    href="mailto:uppionlineacademy@gmail.com"
+                    class="header-contact__link"
+                    >
+                    <i class="fa fa-envelope header-contact__icon"></i>
+
+                    uppionlineacademy@gmail.com</a
+                  >
+                </li>
+                <li class="header-contact__list">
+                  <a
+                    href="https://wa.me/919513009699"
+                    class="header-contact__link"
+                  >
+                  <i class="fa fa-phone header-contact__icon"></i>
+                  +91 953-874-4550
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="body-header-main">
+          <div id="hamberger" class="hamberger">
+            <span class="hamberger__line"></span>
+            <span class="hamberger__line"></span>
+            <span class="hamberger__line"></span>
+          </div>
+          <!-- end hamberger -->
+
+          <header>
+            <div class="header-brand">
+              <a href="<?php base_url();?>index.php">
+                <img
+                  src="<?php base_url();?>img/logo/uppi-logo.jpg"
+                  alt="uppi online academy"
+                />
+              </a>
+            </div>
+            <nav class="header-nav" id="header-nav">
+              <ul>
+                <li class="header-nav__list">
+                  <a href="<?php base_url();?>course.php" class="header-nav__link">courses</a>
+                </li>
+                <li class="header-nav__list">
+                  <a href="<?php base_url();?>notice.php" class="header-nav__link">notice</a>
+                </li>
+                <li class="header-nav__list">
+                  <a href="<?php base_url();?>contact_us.php" class="header-nav__link">contact</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+      </div>
+      <!-- end body header -->
