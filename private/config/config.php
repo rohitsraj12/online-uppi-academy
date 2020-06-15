299127a78ce1c1cc7172395bc9ba70a4a8cafa11
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-15 22:03:17 +0530

    Update config.php

--- private/config/config.php
+++ private/config/config.php
@@ -1,7 +1,5 @@
 <?php
 
 function base_url(){
-    $localhost = "http://localhost/projects/online-uppi-academy/public";
-
-    return $localhost;
+    echo "http://localhost/projects/online-uppi-academy/public/";
 }
