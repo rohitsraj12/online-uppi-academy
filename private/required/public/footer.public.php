cf6140a7f187731f057c5c587e2eafb2bfbf9443
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-15 22:03:36 +0530

    Create footer.public.php

---
+++ private/required/public/footer.public.php
@@ -0,0 +1,116 @@
+
+
+      <div class="body-footer">
+        <footer>
+          <div class="body-footer-top">
+            <div class="footer-brand">
+              <div class="footer-brand-detail">
+                <img
+                  class="footer-brand__image"
+                  src="<?php base_url();?>img/logo/uppi-logo.jpg"
+                  alt="uppi online academy"
+                />
+              </div>
+              <div class="footer-contact">
+                <ul>
+                  <li class="footer-contact__list">
+                    <a href="#" class="footer-contact__link"
+                      >uppionlineacademy@gmail.com</a
+                    >
+                  </li>
+                  <li class="footer-contact__list">
+                    <a href="#" class="footer-contact__link"
+                      >+91 953-874-4550</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="footer-main-nav">
+              <header class="footer__header">
+                quick links
+              </header>
+              <ul>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>index.php" class="footer__link">home</a>
+                </li>
+                <li class="footer__list">
+                  <a href="#" class="footer__link">site map</a>
+                </li>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>contact_us.php" class="footer__link">contact us</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-course-nav">
+              <header class="footer__header">
+                Course
+              </header>
+              <ul>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>course.php" class="footer__link">All courses</a>
+                </li>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>course.php" class="footer__link">Foundation</a>
+                </li>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>course.php" class="footer__link">IIT-JEE</a>
+                </li>
+                <li class="footer__list">
+                  <a href="<?php base_url();?>course.php" class="footer__link">NEET</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-social-media">
+              <header class="footer__header">
+                social media
+              </header>
+              <ul>
+                <li class="footer-social__list">
+                  <a href="https://www.facebook.com/UPPI-Online-Academy-108946954178912/?modal=admin_todo_tour/" class="footer-social__link" target="_blank"
+                    ><i class="fa fa-facebook"></i
+                  ></a>
+                </li>
+                <li class="footer-social__list">
+                  <a href="https://twitter.com/UppiAcademy/" class="footer-social__link" target="_blank"
+                    ><i class="fa fa-twitter"></i
+                  ></a>
+                </li>
+                <li class="footer-social__list">
+                  <a href="https://www.instagram.com/uppionlineacademy/" class="footer-social__link" target="_blank"
+                    ><i class="fa fa-instagram"></i
+                  ></a>
+                </li>
+                <li class="footer-social__list">
+                  <a href="#" class="footer-social__link" target="_blank"
+                    ><i class="fa fa-youtube"></i
+                  ></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="body-footer-bottom">
+            <div class="wrap-container">
+              <div class="copy_rights">
+                <p>
+                  copy right 2020 |
+                  <a href="<?php base_url();?>index.php">uppionlineacademy.com</a>
+                </p>
+              </div>
+
+              <div class="developer-detail">
+                <p>Developed by <a href="#"></a></p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+      <!-- end body footer -->
+    </div>
+
+    <!-- script -->
+    <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
+    <script src="<?php base_url();?>js/owl.carousel.js"></script>
+    <script src="<?php base_url();?>js/script.js"></script>
+  </body>
+</html>
